c642f96703ef4d40d337a0300f5cc4b429017e75
farhan <mfarhan133@gmail.com>
2020-04-26 18:02:32 +0500

    Add logger Add movies filtering by search, languages and category.

--- Task4_ImdbClone/src/movie/urls.py
+++ Task4_ImdbClone/src/movie/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MovieList, MovieDetail
+from .views import *
 
 app_name='movie'
 
 urlpatterns = [
     path('', MovieList.as_view(), name="movie_list"),
     path('<int:pk>', MovieDetail.as_view(), name="movie_detail"),
+    path('category/<str:category>', MovieCategory.as_view(), name="movie_category"),
+    path('language/<str:language>', MovieLanguage.as_view(), name="movie_language"),
+    path('search/', MovieSearch.as_view(), name="movie_search"),
 ]
--- Task4_ImdbClone/src/movie/views.py
+++ Task4_ImdbClone/src/movie/views.py
@@ -1,13 +1,17 @@
+import logging
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.dates import YearArchiveView
 from .models import Movie, MovieLinks
 
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 class MovieList(ListView):
     model = Movie
-    paginate_by = 1
+    paginate_by = 2
 
 
 class MovieDetail(DetailView):
@@ -25,3 +29,50 @@ class MovieDetail(DetailView):
         # context['related_movies'] = Movie.objects.filter(
         #     category=self.get_object().category)  # .order_by['created'][0:6]
         return context
+
+
+class MovieCategory(ListView):
+    model = Movie
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.category = self.kwargs['category']
+        # Testing logger
+        logger.info(
+            '>>>> In MovieCategory > get_queryset , cat=' + self.category)
+        return Movie.objects.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieCategory, self).get_context_data(**kwargs)
+        context['movie_category'] = self.category
+        return context
+
+
+class MovieLanguage(ListView):
+    model = Movie
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.language = self.kwargs['language']
+        return Movie.objects.filter(language=self.language)
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieLanguage, self).get_context_data(**kwargs)
+        context['movie_language'] = self.language
+        return context
+
+
+class MovieSearch(ListView):
+    model = Movie
+    paginate_by = 2
+
+    def get_queryset(self):
+        # query = self.request.GET.get('query', '')
+        # return Movie.objects.filter(title__icontains=query)
+        query = self.request.GET.get('query')
+        if query:
+            object_list = self.model.objects.filter(title__icontains=query)
+        else:
+            object_list = self.model.objects.none()
+        return object_list
+
